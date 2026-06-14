9a55c7de0aa565bd527442ffa093ab895a194b2d
ayaandube2012-goolor <ayaandube2012@gmail.com>
2026-06-14 19:12:10 +0100

    Update email functionality for weekly motivation
    Refactor email sending logic to include motivational quotes and change birthday notification to a weekly schedule.

--- main.py
+++ main.py
@@ -1,38 +1,26 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
+import datetime as dt
 import smtplib
-import os
-
-# import os and use it to get the Github repository secrets
+import random
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
-
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
-
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+now = dt.datetime.now()
+day = now.weekday()
+if day == 0:
+    with open("quotes.txt", "r") as quotes:
+        quotes_str = quotes.read()
+        quotes_list = quotes_str.splitlines()
+    quote = random.choice(quotes_list)
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs="24AyaanDube@lgs.slough.sch.uk",
+                            msg = "Subject: Good morning\n"
+                            f"From: {MY_EMAIL}\n"
+                            "To: 24AyaanDube@lgs.slough.sch.uk\n"
+                            "\n"
+                            "Hello,\n"
+                            "I know this is probably the day you hate the most so here's some motivation:\n"
+                            f"{quote}")
+        print("email sent")
